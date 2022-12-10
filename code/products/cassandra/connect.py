3c754c8aefa1fd0e7dc01cdc9612f88758d03aac
laysa.uchoa <laysa.uchoa@aiven.io>
2022-12-10 16:30:43 +0100

    Fix bug on the code

--- code/products/cassandra/connect.py
+++ code/products/cassandra/connect.py
@@ -5,8 +5,13 @@ from cassandra.policies import DCAwareRoundRobinPolicy
 
 auth_provider = PlainTextAuthProvider(USER, PASSWORD)
 ssl_options = {"ca_certs": SSL_CERTFILE, "cert_reqs": ssl.CERT_REQUIRED}
-with Cluster([HOST], port=PORT, ssl_options=ssl_options, auth_provider=auth_provider, 
-                    load_balancing_policy=DCAwareRoundRobinPolicy(local_dc='aiven')) as cluster:
-        with cluster.connect() as session:
-        # Here you can now read from or write to the database
-
+with Cluster(
+    [HOST],
+    port=PORT,
+    ssl_options=ssl_options,
+    auth_provider=auth_provider,
+    load_balancing_policy=DCAwareRoundRobinPolicy(local_dc="aiven"),
+) as cluster:
+    with cluster.connect() as session:
+        pass
+    # Here you can now read from or write to the database
